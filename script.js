fcbd24234c397a4d2669af9881b0c59bbd5876d8
arek <arekmod98@gmail.com>
2022-05-15 21:47:52 +0200

    event listener that console logs button text

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-function add(a, b) {
-    return a + b;
-}
-function subtract(a, b) {
-    return a - b;
-}
-function multiply(a, b) {
-    return a * b;
-}
-function divide(a, b) {
-    return a / b;
-}
+function add(a, b) { return a + b; }
+
+function subtract(a, b) { return a - b; }
+
+function multiply(a, b) { return a * b; }
+
+function divide(a, b) { return a / b; }
+
 function operate(operator, a, b) {
-    if(operator=='+') return add(a, b);
-    if(operator=='-') return subtract(a, b);
-    if(operator=='*') return multiply(a, b);
-    if(operator=='/') return divide(a, b);
+    if (operator == '+') return add(a, b);
+    if (operator == '-') return subtract(a, b);
+    if (operator == '*') return multiply(a, b);
+    if (operator == '/') return divide(a, b);
+}
+
+const button = document.querySelectorAll('button');
+
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', () => {
+        console.log(button[i].innerText)
+    })
 }
