faec8b722860b1a5b26503418f21509312041ce7
arek <arekmod98@gmail.com>
2022-05-17 00:39:29 +0200

    adds event listener for clear button that clears the display

--- script.js
+++ script.js
@@ -12,13 +12,19 @@ function operate(operator, a, b) {
     if (operator == '*') return multiply(a, b);
     if (operator == '/') return divide(a, b);
 }
-
-const number = document.querySelectorAll('.number');
+const clear = document.querySelector('.clear')
+const number = document.querySelectorAll('.number')
 const display_operation = document.querySelector('.operation')
+const display_result = document.querySelector('.result')
+
+clear.addEventListener('click', () => {
+    display_operation.innerText = ''
+    display_result.innerText = ''
+})
 
 for (let i = 0; i < number.length; i++) {
     number[i].addEventListener('click', () => {
         let display_value = number[i].innerText
-        display_operation.innerText = display_value
+        display_operation.innerText += display_value
     })
 }
