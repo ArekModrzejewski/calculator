4c07c12242aba604cb3347193fe1dd16f72b8684
arek <arekmod98@gmail.com>
2022-05-18 14:43:19 +0200

    change loops to forEach, improve backspace and clear

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const clear = document.querySelector('.clear')
+const backspace = document.querySelector('.backspace')
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+const display_operation = document.querySelector('.operation')
+const display_result = document.querySelector('.result')
+
 function add(a, b) { return a + b; }
 
 function subtract(a, b) { return a - b; }
@@ -12,24 +19,28 @@ function operate(operator, a, b) {
     if (operator == '*') return multiply(a, b);
     if (operator == '/') return divide(a, b);
 }
-const clear = document.querySelector('.clear')
-const backspace = document.querySelector('.backspace')
-const number = document.querySelectorAll('.number')
-const display_operation = document.querySelector('.operation')
-const display_result = document.querySelector('.result')
 
 backspace.addEventListener('click', () => {
-    display_result.innerText = display_result.innerText.slice(0, -1)
+    if (display_result.innerText != 0) {
+        display_result.innerText = display_result.innerText.slice(0, -1)
+    }
+    if (display_result.innerText == '') {display_result.innerText = 0}
 })
 
 clear.addEventListener('click', () => {
     display_operation.innerText = ''
-    display_result.innerText = ''
+    display_result.innerText = '0'
 })
 
-for (let i = 0; i < number.length; i++) {
-    number[i].addEventListener('click', () => {
-        let display_value = number[i].innerText
-        display_result.innerText += display_value
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        if (display_result.innerText == 0) { display_result.innerText = '' }
+        display_result.innerText += number.innerText
     })
-}
+})
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        display_operation.innerText = display_result.innerText + ' ' + operator.innerText
+        display_result.innerText = 0
+    })
+})
